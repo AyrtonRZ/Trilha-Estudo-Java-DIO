34a698defecf72872e9489ebe73ac01c9b115eb7
AyrtonRZ <ayrtonrodrigues45@gmail.com>
2023-12-09 22:35:00 -0300

    feta: Add DeclaracaoMetodo class with main method

---
+++ src/DeclaracaoMetodo.java
@@ -0,0 +1,14 @@
+public class DeclaracaoMetodo {
+    public static void main(String[] args) {
+        String nome = "João";
+        String sobrenome = "Silva";
+
+        String nomeCompleto = nomeCompleto(nome, sobrenome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String nome, String sobrenome) {
+        return "Olá, " + nome.concat(" ").concat(sobrenome);
+    }
+}
