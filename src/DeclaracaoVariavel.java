92705cb2b80b8b4a140f95ce69c8b7795d3e4e2b
AyrtonRZ <ayrtonrodrigues45@gmail.com>
2023-12-09 21:37:12 -0300

    feat: adiciona primerio commit

---
+++ src/DeclaracaoVariavel.java
@@ -0,0 +1,23 @@
+public class DeclaracaoVariavel {
+    public static void main(String[] args) {
+        System.out.println("Olá, mundo!"); 
+
+        // Declaração de variáveis
+
+        String meuNome = "João";
+        int minhaIdade = 20;
+        double meuPeso = 70.5;
+        boolean souEstudante = true;
+
+        System.out.println("Meu nome é " + meuNome);
+        System.out.println("Tenho " + minhaIdade + " anos");
+        System.out.println("Peso " + meuPeso + " kg");
+        
+        // Condicional if-else
+        if (souEstudante) {
+            System.out.println("Sou estudante");
+        } else {
+            System.out.println("Não sou estudante");
+        }
+    }
+}
