67922f3cf2107ad9bc4348d0436d89543810d69f
AyrtonRZ <ayrtonrodrigues45@gmail.com>
2023-12-09 22:21:42 -0300

    feat: Add DeclaracaoVariavel class with variable declarations

---
+++ src/DeclaracaoVariavel.java
@@ -0,0 +1,13 @@
+public class DeclaracaoVariavel {
+    public static void main(String[] args) {
+        int idade = 20;
+        double salario = 1000.50;
+        String nome = "Gustavo";
+        boolean verdadeiro = true;
+        boolean falso = false;
+        System.out.println("A idade é " + idade);
+        System.out.println("O salário é " + salario);
+        System.out.println("O nome é " + nome);
+
+    }
+}
